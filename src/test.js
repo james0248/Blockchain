74473e1bd828cf9de9858cf35d72be9c58d1021c
james0248 <hsjames0248@gmail.com>
2020-02-05 01:34:04 +0900

    Add tests for PoW function

--- src/test.js
+++ src/test.js
@@ -1,12 +1,29 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const Blockchain = require('./blockchain.js');
 
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(1231, 'sdasdadadsads', 'adasdad');
-bitcoin.createNewTransaction(1, 'A', 'B');
-bitcoin.createNewBlock(2817, 'kdkhkahwn', 'dkjahdkhw');
-bitcoin.createNewTransaction(27191, 'A', 'B');
-bitcoin.createNewTransaction(2981, 'B', 'C');
-bitcoin.createNewTransaction(1817231273, 'ja', 'jkk');
-bitcoin.createNewBlock(2817, 'kdkhkahwn', 'dkjahdkhw');
-console.log(bitcoin);
+let previousBlockHash = 'shadkh3y78y2398ws98dyq823';
+let currentBlockData = [
+    {
+        amount: 101,
+        sender: '78atw3gw8fao783g2',
+        recipient: '902uns98yq8o82zm3eoa28y'
+    },
+    {
+        amount: 287,
+        sender: 'ahfra9wu3m9x498w39r',
+        recipient: 'WNEXRYAW3M94Y9QY23'
+    },
+    {
+        amount: 8271,
+        sender: 'ajdisrnyw83ymox3xq',
+        recipient: 'ahdh38Q7YN4I3I2NI'
+    }
+];
+let difficulty = 5;
+
+it('Should have 0s in the beginning', (done) => {
+    expect(bitcoin.proofOfWork(previousBlockHash, currentBlockData, difficulty).substring(0, difficulty))
+    .to.be.string('0'.repeat(difficulty));
+    done();
+}).timeout(100000);
