7f2a56fcd3b8c019ed8b7fccd93e7a225478dafa
james0248 <hsjames0248@gmail.com>
2020-02-05 01:34:23 +0900

    Add full functionalities to Blockchain module

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,8 +1,10 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Blockchain {
     constructor() {
         this.chain = [];
         this.transaction = [];
-        this.pendingTransactions = [];
+        this.createNewBlock(0, '0', '0');
     }
 
     createNewBlock(nonce, previousBlockHash, hash) {
@@ -33,8 +35,20 @@ class Blockchain {
         return this.getLastBlock()['index'] + 1; // returns the number of block which contains this transaction
     }
 
-    hashBlock() {
+    hashBlock(previousBlockHash, currentBlockData, nonce) {
+        const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        const hash = SHA256(dataAsString).toString();
+        return hash;
+    }
 
+    proofOfWork(previousBlockHash, currentBlockData, difficulty) {
+        let nonce = 0;
+        let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        while(hash.substring(0, difficulty) !== '0'.repeat(difficulty)) {
+            nonce++;
+            hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        }
+        return hash;
     }
 }
 
