f67e013432ea1cfde6706b5d143637a6f5c2e16a
james0248 <hsjames0248@gmail.com>
2020-02-04 12:55:56 +0900

    Add making new transactions functionality to blockchain

--- src/blockchain.js
+++ src/blockchain.js
@@ -2,20 +2,39 @@ class Blockchain {
     constructor() {
         this.chain = [];
         this.transaction = [];
+        this.pendingTransactions = [];
     }
 
     createNewBlock(nonce, previousBlockHash, hash) {
         const newBlock = {
             index: this.chain.length + 1,
             timestamp: Date.now(),
-            transactions: this.newTransaction,
+            transactions: this.pendingTransactions,
             nonce: nonce,
             hash: hash,
             previousBlockHash: previousBlockHash
         };
-        this.newTransaction = []; // Clear out new transactions for the next block
+        this.pendingTransactions = []; // Clear out new transactions for the next block
         this.chain.push(newBlock);
         return newBlock;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    createNewTransaction(amount, sender, recipient) {
+        const newTransaction = {
+            amount: amount,
+            sender, sender,
+            recipient: recipient
+        };
+        this.pendingTransactions.push(newTransaction);
+        return this.getLastBlock()['index'] + 1; // returns the number of block which contains this transaction
+    }
+
+    hashBlock() {
+
     }
 }
 
--- src/test.js
+++ src/test.js
@@ -1,8 +1,12 @@
+const assert = require('assert');
 const Blockchain = require('./blockchain.js');
 
 const bitcoin = new Blockchain();
-
 bitcoin.createNewBlock(1231, 'sdasdadadsads', 'adasdad');
-bitcoin.createNewBlock(1231, 'sdasdadadsads', 'adasdad');
-bitcoin.createNewBlock(1231, 'sdasdadadsads', 'adasdad');
+bitcoin.createNewTransaction(1, 'A', 'B');
+bitcoin.createNewBlock(2817, 'kdkhkahwn', 'dkjahdkhw');
+bitcoin.createNewTransaction(27191, 'A', 'B');
+bitcoin.createNewTransaction(2981, 'B', 'C');
+bitcoin.createNewTransaction(1817231273, 'ja', 'jkk');
+bitcoin.createNewBlock(2817, 'kdkhkahwn', 'dkjahdkhw');
 console.log(bitcoin);
